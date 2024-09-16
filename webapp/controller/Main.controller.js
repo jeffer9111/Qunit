eeda26370213e348f7b00cf439f48ce237589b81
jeffer9111 <jjeffer_ra@hotmail.com>
2024-09-16 19:58:03 +0000

    QUnit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("student.com.sap.training.ux402.qunit.ux402qunit.controller.Main", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"student/com/sap/training/ux402/qunit/ux402_qunit/test/unit/controller/Main.controller",
+	"student/com/sap/training/ux402/qunit/ux402_qunit/test/unit/model/Formatter"
+],
+	function () {
+		"use strict";
+	});
